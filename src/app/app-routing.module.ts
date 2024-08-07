31c6b0b18d296ca1020a0d2334e2c842d517de75
JBorad988 <jayborad366@gmail.com>
2024-08-07 17:39:34 +0530

    init2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CitySwapperComponent} from "./city-swapper/city-swapper.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: CitySwapperComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
